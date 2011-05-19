6976ca7c6f71b52b6e103180e1e12e427d9edd53
lucastex <lucastex@gmail.com>
2011-05-19 00:43:55 -0300

    changed declaration for groovy file

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,4 +1,18 @@
 // Place your Spring DSL code here
 beans = {
 
+	xmlns task: "http://www.springframework.org/schema/task"
+
+	task.'scheduled-tasks' {
+		
+		//using cron expression
+		task.scheduled(ref: 'executorService', method: 'exec', cron: '* * * * * ?')
+		
+		//using fixedRate (time between executions start time)
+		//task.scheduled(ref: 'executorService', method: 'exec', 'fixed-rate': 5000L)
+
+		//using fixedDelay (time between executions finish time)
+		//task.scheduled(ref: 'executorService', method: 'exec', 'fixed-delay': 5000L)
+	}
+	
 }
